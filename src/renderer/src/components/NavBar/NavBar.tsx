449a020aa3e04e18de95b3482858f42ac474c17e
Samarkin Danil <samarkin.danil@yandex.ru>
2024-08-06 23:11:20 +0300

    feat: set active to timer if id in location

--- src/renderer/src/components/NavBar/NavBar.tsx
+++ src/renderer/src/components/NavBar/NavBar.tsx
@@ -1,25 +1,29 @@
 import { ReactElement } from 'react'
 import NavBarItem from './NavBarItem'
 import ButtonUI from '../UI/ButtonUI'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import HomeSvg from '../../assets/Icons/Home.svg'
 import { useTimersStore } from '../../store/timersStore'
 
 const NavBar = (): ReactElement => {
   const timerStore = useTimersStore()
   const timers = timerStore.timers
+  const location = useLocation()
+  const timerIsActive = (timerId: string): boolean => {
+    return location.pathname.includes(timerId)
+  }
 
   return (
     <nav className="shrink-0 overflow-hidden bg-[#f2f4f5] gap-y-6 min-w-[230px] basis-[30%] py-3 pl-3 flex flex-col justify-between">
       <div>
-        <Link className="flex items-center gap-x-2 mb-2 p-2 bg-white rounded-md mr-2" to="/">
+        <Link className="flex items-center gap-x-2 mb-2 p-2 bg-white rounded-md mr-3" to="/">
           <img className="h-4 w-4" src={HomeSvg} alt="home" />
           <span>Home page</span>
         </Link>
         <ul className="flex flex-col gap-y-1 overflow-y-auto">
           {timers.map((timer) => (
             <li key={timer.id} className="mr-3">
-              <NavBarItem timer={timer} />
+              <NavBarItem active={timerIsActive(timer.id)} timer={timer} />
             </li>
           ))}
         </ul>
