d83631939ad13f4e7873ae4d6d4d80050d1ecbfb
Samarkin Danil <samarkin.danil@yandex.ru>
2024-08-06 16:30:45 +0300

    style: new scroll bar style

--- src/renderer/src/components/NavBar/NavBar.tsx
+++ src/renderer/src/components/NavBar/NavBar.tsx
@@ -4,15 +4,9 @@ import ButtonUI from '../UI/ButtonUI'
 
 const NavBar = (): ReactElement => {
   return (
-    <nav className="shrink-0 bg-[#f2f4f5] max-w-[230px] basis-[30%] p-3 flex flex-col justify-between">
-      <ul className="flex flex-col gap-y-1">
-        <li>
-          <NavBarItem />
-        </li>
-        <li>
-          <NavBarItem />
-        </li>
-        <li>
+    <nav className="shrink-0 overflow-hidden bg-[#f2f4f5] gap-y-6 max-w-[230px] basis-[30%] py-3 pl-3 flex flex-col justify-between">
+      <ul className="flex flex-col gap-y-1 overflow-y-auto">
+        <li className="mr-3">
           <NavBarItem active />
         </li>
       </ul>
